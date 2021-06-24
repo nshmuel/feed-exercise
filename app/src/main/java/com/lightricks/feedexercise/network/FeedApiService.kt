83608b6c4020eedbca5dbf22d300107df3322efd
Nir Shmuel <nshmuel@lightricks.com>
2021-06-24 18:07:52 +0300

    FeedApiService: Create and implement FeedApiService interface
    Created an interface called FeedApiService with a method that executes an Http GET request
    and returns an RxJava’s Single that contains the response.
    Used Retrofit and Moshi to implement the FeedApiService interface.
    
    FeedFragment: Revert changes

--- app/src/main/java/com/lightricks/feedexercise/network/FeedApiService.kt
+++ app/src/main/java/com/lightricks/feedexercise/network/FeedApiService.kt
@@ -1,5 +1,39 @@
 package com.lightricks.feedexercise.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import io.reactivex.rxjava3.core.Single
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
 /**
  * todo: add the FeedApiService interface and the Retrofit and Moshi code here
  */
+
+interface FeedApiService {
+    @GET(value = "/Android/demo/feed.json")
+    fun getFeed(): Single<GetFeedResponseDTO>
+
+    companion object{
+        private const val BASE_URL = "https://assets.swishvideoapp.com"
+        const val THUMBNAIL_PREFIX  = "$BASE_URL/Android/demo/catalog/thumbnails/"
+
+        val instance by lazy { create() }
+
+         private fun create(): FeedApiService {
+            val moshi = Moshi.Builder()
+                .addLast(KotlinJsonAdapterFactory())
+                .build()
+
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                .build()
+
+            return retrofit.create(FeedApiService::class.java)
+        }
+    }
+}
